ee2a480774e2f4a70981180b0421f4af642d0532
emmaexe <42188518+emmaexe@users.noreply.github.com>
2024-09-07 22:07:00 +0200

    [fix] Remove dangling refrence and seperate NotificationActions by type

--- app/src/NotificationManager/NotificationManager.cpp
+++ app/src/NotificationManager/NotificationManager.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 
 NotificationAction::NotificationAction(const nlohmann::json& actionData) {
+    this->type = NotificationActionType::BUTTON;
     try {
         if (actionData["action"] == "view") {
             this->label = actionData["label"];
@@ -19,12 +20,13 @@ NotificationAction::NotificationAction(const nlohmann::json& actionData) {
 }
 
 NotificationAction::NotificationAction(const std::string& clickUrl) {
+    this->type = NotificationActionType::CLICK;
     this->label = "Open URL";
     this->url = clickUrl;
     this->useable = true;
 }
 
-void NotificationManager::generalNotification(const std::string& title, const std::string& message, std::optional<NotificationPriority> priority, std::optional<NotificationAttachment> attachment, std::optional<std::vector<NotificationAction>> actions) {
+void NotificationManager::generalNotification(const std::string title, const std::string message, std::optional<NotificationPriority> priority, std::optional<NotificationAttachment> attachment, std::optional<std::vector<NotificationAction>> actions) {
     KNotification* notification = new KNotification("general");
     notification->setTitle(title.c_str());
     notification->setText(message.c_str());
@@ -34,14 +36,21 @@ void NotificationManager::generalNotification(const std::string& title, const st
     }
     if (attachment.has_value()) {
         // TODO: Check if attachment is native to the ntfy server, or if it is external and proxy it (?)
-        notification->setUrls(QList<QUrl>({ QUrl(attachment->url.c_str()) }));
+        notification->setUrls(QList<QUrl>({ QUrl(QString::fromStdString(attachment->url)) }));
     }
     if (actions.has_value()) {
         for (NotificationAction action: actions.value()) {
             if (!action.useable) { continue; }
-            KNotificationAction* knaction = notification->addAction(action.label.c_str());
-            KNotificationAction::connect(knaction, &KNotificationAction::activated, [&](){
-                QDesktopServices::openUrl(QUrl(action.url.c_str()));
+            KNotificationAction* knaction;
+
+            if (action.type == NotificationActionType::CLICK) {
+                knaction = notification->addDefaultAction(QString::fromStdString(action.label));
+            } else if (action.type == NotificationActionType::BUTTON) {
+                knaction = notification->addAction(QString::fromStdString(action.label));
+            }
+            
+            KNotificationAction::connect(knaction, &KNotificationAction::activated, [actionUrl = action.url](){
+                QDesktopServices::openUrl(QUrl(QString::fromStdString(actionUrl)));
             });
         }
     }
