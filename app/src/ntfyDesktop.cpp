6fdf59f54eed29b83c0c63bbddc72345f7d2b2e6
emmaexe <42188518+emmaexe@users.noreply.github.com>
2024-09-14 13:15:05 +0200

    [fix] Change KAboutData to correct componentName, update its contents.

--- app/src/ntfyDesktop.cpp
+++ app/src/ntfyDesktop.cpp
@@ -23,13 +23,21 @@ int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
 
     KLocalizedString::setApplicationDomain("moe.emmaexe.ntfyDesktop");
-    KAboutData aboutData(QStringLiteral("ntfyDesktop"), i18n("Ntfy Desktop"), QStringLiteral(ND_VERSION), i18n("A client for the ntfy notification service."), KAboutLicense::GPL_V3, i18n("(c) 2024"), i18n("Some text..."), QStringLiteral("https://github.com/emmaexe/ntfyDesktop"), QStringLiteral("https://github.com/emmaexe/ntfyDesktop/issues"));
-    aboutData.setDesktopFileName("moe.emmaexe.ntfyDesktop");
-    aboutData.addAuthor(i18n("emmaexe"), i18n("Author"), QStringLiteral(""), QStringLiteral("https://www.emmaexe.moe/"), QStringLiteral(""));
+    KAboutData aboutData(
+        QStringLiteral("moe.emmaexe.ntfyDesktop"),
+        QStringLiteral("Ntfy Desktop"),
+        QStringLiteral(ND_VERSION),
+        i18n(ND_DESCRIPTION_SUMMARY),
+        KAboutLicense::GPL_V3,
+        QStringLiteral("(c) 2024"),
+        QStringLiteral(),
+        QStringLiteral(ND_HOMEPAGE_URL),
+        QStringLiteral(ND_ISSUES_URL)
+    );
+    aboutData.addAuthor(QStringLiteral("emmaexe"), i18n("Author"), QStringLiteral(""), QStringLiteral("https://www.emmaexe.moe/"), QStringLiteral("https://cdn.discordapp.com/avatars/299265668522442752/841bddd42846b31f2a06eeb5e93a0652.png?size=4096"));
     KAboutData::setApplicationData(aboutData);
 
     QCommandLineParser parser;
-
     parser.addPositionalArgument("url", "Optional URL argument. Used by x-scheme-handler to handle the ntfy:// protocol.", "[url]");
 
     aboutData.setupCommandLine(&parser);
@@ -37,11 +45,9 @@ int main(int argc, char* argv[]) {
     aboutData.processCommandLine(&parser);
 
     std::optional<std::string> passedUrl = std::nullopt;
-
     if (!parser.positionalArguments().empty()) {
         passedUrl = parser.positionalArguments().first().toStdString();
     }
-
     SingleInstanceManager singleInstanceManager([&](std::optional<std::string> url){ std::cerr << "A new instance was started, but this instance does not have a main window to show." << std::endl; }, passedUrl);
 
     std::srand(std::time(0));
