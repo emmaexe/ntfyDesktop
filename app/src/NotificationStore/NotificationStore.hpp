ef08899a6c0ee5ac81e3e58babb672b901763cd4
emmaexe <42188518+emmaexe@users.noreply.github.com>
2024-09-15 21:24:24 +0200

    [feat] Add NotificationStore to link domains/topics with the ID of the last seen notification for those domains/topics

---
+++ app/src/NotificationStore/NotificationStore.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <map>
+#include <optional>
+#include <string>
+
+/**
+ * @brief A static global class, holds the last seen notification.
+ */
+class NotificationStore {
+    public:
+        NotificationStore() = delete;
+        /**
+         * @brief Update a domain/topic with the ID of the last seen notification.
+         *
+         * @param domain The domain to update
+         * @param topic The topic to update
+         * @param notificationID The ID of the last seen notification for that domain/topic
+         */
+        static void update(const std::string& domain, const std::string& topic, const std::string& notificationID = "all");
+        /**
+         * @brief Get the last seen notification ID for some domain/topic.
+         *
+         * @param domain The domain to get
+         * @param topic The topic to get
+         * @return std::optional<std::string> - The ID of the last seen notification for that domain/topic. If there is no ID, returns `std::nullopt`.
+         */
+        static std::optional<std::string> get(const std::string& domain, const std::string& topic);
+        /**
+         * @brief Check if a last seen notification ID for some domain/topic exists.
+         *
+         * @param domain The domain to check
+         * @param topic The topic to check
+         */
+        static bool exists(const std::string& domain, const std::string& topic);
+        /**
+         * @brief Remove a domain/topic from the NotificationStore.
+         *
+         * @param domain The domain to remove
+         * @param topic The topic to remove
+         */
+        static void remove(const std::string& domain, const std::string& topic);
+    private:
+        static bool initialized;
+        static std::map<std::string, std::string> internalData;
+        static void read();
+        static void write();
+        static const std::string getStorePath();
+        static const std::string getStoreFile();
+};
