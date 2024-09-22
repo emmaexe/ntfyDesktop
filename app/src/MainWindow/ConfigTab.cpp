65c0cd1c0fd5e0c377753f9fbf7736571df21b59
emmaexe <42188518+emmaexe@users.noreply.github.com>
2024-09-22 13:24:32 +0200

    [feat] Add a "secure" toggle to the UI

--- app/src/MainWindow/ConfigTab.cpp
+++ app/src/MainWindow/ConfigTab.cpp
@@ -2,17 +2,20 @@
 
 #include "ui_ConfigTab.h"
 
-ConfigTab::ConfigTab(std::string name, std::string server, std::string topic, QWidget* parent): QWidget(parent), ui(new Ui::ConfigTab) {
+ConfigTab::ConfigTab(std::string name, std::string server, std::string topic, bool secure, QWidget* parent): QWidget(parent), ui(new Ui::ConfigTab) {
     this->ui->setupUi(this);
-    this->ui->nameLineEdit->setText(name.c_str());
-    this->ui->serverLineEdit->setText(server.c_str());
-    this->ui->topicLineEdit->setText(topic.c_str());
+    this->ui->nameLineEdit->setText(QString::fromStdString(name));
+    this->ui->domainLineEdit->setText(QString::fromStdString(server));
+    this->ui->topicLineEdit->setText(QString::fromStdString(topic));
+    this->ui->secureCheckBox->setChecked(secure);
 }
 
 ConfigTab::~ConfigTab() { delete ui; }
 
 std::string ConfigTab::getName() { return this->ui->nameLineEdit->text().toStdString(); }
 
-std::string ConfigTab::getServer() { return this->ui->serverLineEdit->text().toStdString(); }
+std::string ConfigTab::getDomain() { return this->ui->domainLineEdit->text().toStdString(); }
 
 std::string ConfigTab::getTopic() { return this->ui->topicLineEdit->text().toStdString(); }
+
+bool ConfigTab::getSecure() { return this->ui->secureCheckBox->isChecked(); }
--- app/src/MainWindow/ConfigTab.hpp
+++ app/src/MainWindow/ConfigTab.hpp
@@ -11,11 +11,12 @@ QT_END_NAMESPACE
 class ConfigTab: public QWidget {
         Q_OBJECT
     public:
-        ConfigTab(std::string name, std::string server, std::string topic, QWidget* parent = nullptr);
+        ConfigTab(std::string name = "", std::string server = "", std::string topic = "", bool secure = true, QWidget* parent = nullptr);
         ~ConfigTab();
         std::string getName();
-        std::string getServer();
+        std::string getDomain();
         std::string getTopic();
+        bool getSecure();
     private:
         Ui::ConfigTab* ui;
 };
