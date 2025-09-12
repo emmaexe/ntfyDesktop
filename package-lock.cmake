e8dc5b6b7711f435950b48dcfde8fcbd63a6ba20
Emma <contact@emmaexe.moe>
2025-09-12 21:22:39 +0200

    Add CPM Package Lock

---
+++ package-lock.cmake
@@ -0,0 +1,17 @@
+# CPM Package Lock
+# This file should be committed to version control
+
+# json
+CPMDeclarePackage(json
+  VERSION 3.12.0
+  GITHUB_REPOSITORY nlohmann/json
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
+)
+# emojicpp
+CPMDeclarePackage(emojicpp
+  VERSION 2.0.1
+  GITHUB_REPOSITORY emmaexe/emojicpp
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
+)
